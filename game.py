eec1da0415da68a32b86673fdecb56b238489300
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-05-12 22:46:06 -0700

    cloud movement via key input

--- game.py
+++ game.py
@@ -16,10 +16,15 @@ class Game:
 
         self.img = pygame.image.load('data/images/clouds/cloud_1.png')
 
+        self.img_pos = [160, 260]  # x,y
+        self.movement = [False, False]
+
     def run(self):
         # Important to remember: each frame is an iteration in a loop, so dynamic sleep
         while True:
-            self.screen.blit(self.img, (100, 200))  # x=100, y=200
+            self.img_pos[1] += self.movement[1] - \
+                self.movement[0]  # T/F as boolean 1's/0's
+            self.screen.blit(self.img, self.img_pos)
 
             # User input
             for event in pygame.event.get():
@@ -27,6 +32,20 @@ class Game:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+
+                # User presses down on a key
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = True
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = True
+
+                # User lifts up from a key
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = False
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = False
 
             pygame.display.update()
             self.clock.tick(60)  # 60 FPS
