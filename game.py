319c2e5bae5785b6817c99e8f2c9fcb1921e4c1d
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-05-13 00:20:49 -0700

    detect rectangle collision area

--- game.py
+++ game.py
@@ -20,6 +20,8 @@ class Game:
         self.img_pos = [160, 260]  # x,y
         self.movement = [False, False]
 
+        self.collision_area = pygame.Rect(50, 50, 300, 50)
+
     def run(self):
         # Important to remember: each frame is an iteration in a loop, so dynamic sleep
         while True:
@@ -29,6 +31,15 @@ class Game:
             self.img_pos[1] += self.movement[1] - \
                 self.movement[0]  # T/F as boolean 1's/0's
             self.screen.blit(self.img, self.img_pos)
+
+            img_r = pygame.Rect(
+                self.img_pos[0], self.img_pos[1], self.img.get_width(), self.img.get_height())
+            if img_r.colliderect(self.collision_area):
+                pygame.draw.rect(self.screen, (0, 100, 255),
+                                 self.collision_area)
+            else:
+                pygame.draw.rect(self.screen, (0, 50, 155),
+                                 self.collision_area)
 
             # User input
             for event in pygame.event.get():
