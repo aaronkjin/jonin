0e46ba27b19386601301540be86df49e99de5d39
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-05-13 22:36:49 -0700

    movement + rendering

--- scripts/entities.py
+++ scripts/entities.py
@@ -8,3 +8,14 @@ class PhysicsEntity:
         self.pos = list(pos)  # S.t. each entity has its own list for positions
         self.size = size
         self.velocity = [0, 0]  # Derivative of position
+
+    def update(self, movement=(0, 0)):
+        frame_movement = (movement[0] + self.velocity[0],
+                          movement[1] + self.velocity[1])
+
+        # Change x, y position based on movement changes
+        self.pos[0] += frame_movement[0]
+        self.pos[1] += frame_movement[1]
+
+    def render(self, surf):
+        surf.blit(self.game.assets['player'], self.pos)
