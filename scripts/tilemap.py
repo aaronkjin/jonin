0c33aefad26389722a18ca62fab6bd7bbea3caa0
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-05-14 20:12:49 -0700

    tile metadata example

--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -3,3 +3,10 @@ class Tilemap:
         self.tile_size = tile_size
         self.tilemap = {}
         self.offgrid_tiles = []
+
+        for i in range(10):
+            # Tile as a dictionary of metadata
+            self.tilemap[str(
+                3 + i) + ";10"] = {"type": "grass", "variant": 1, "pos": (3 + i, 10)}
+            self.tilemap["10;" + str(
+                5 + i)] = {"type": "stone", "variant": 1, "pos": (10, i + 5)}
