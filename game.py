3907eb03527058853751683e1fd17847644a1133
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-05-17 12:06:08 -0700

    oops camera y-axis movement

--- game.py
+++ game.py
@@ -45,8 +45,8 @@ class Game:
             # Camera movement in relation to player
             self.scroll[0] += (self.player.rect().centerx -
                                self.display.get_width() / 2 - self.scroll[0]) / 30
-            self.scroll[1] += (self.player.rect().centerx -
-                               self.display.get_width() / 2 - self.scroll[1]) / 30
+            self.scroll[1] += (self.player.rect().centery -
+                               self.display.get_height() / 2 - self.scroll[1]) / 30
 
             self.tilemap.render(self.display, offset=self.scroll)
 
