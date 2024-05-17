2ce4e0bc79bd357a3f29703aefd0e7628b4bad3f
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-05-16 23:43:33 -0700

    collision dict

--- scripts/entities.py
+++ scripts/entities.py
@@ -8,12 +8,16 @@ class PhysicsEntity:
         self.pos = list(pos)  # S.t. each entity has its own list for positions
         self.size = size
         self.velocity = [0, 0]  # Derivative of position
+        self.collisions = {"up": False, "down": False,
+                           "right": False, "left": False}
 
     def rect(self):
         # Four corners to create the rect
         return pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
 
     def update(self, tilemap, movement=(0, 0)):
+        self.collisions = {"up": False, "down": False,
+                           "right": False, "left": False}
         frame_movement = (movement[0] + self.velocity[0],
                           movement[1] + self.velocity[1])
 
@@ -27,8 +31,10 @@ class PhysicsEntity:
             if entity_rect.colliderect(rect):
                 if frame_movement[0] > 0:  # Right
                     entity_rect.right = rect.left
+                    self.collisions["right"] = True
                 if frame_movement[0] < 0:  # Left
                     entity_rect.left = rect.right
+                    self.collisions["left"] = True
 
                 self.pos[0] = entity_rect.x
 
@@ -42,13 +48,19 @@ class PhysicsEntity:
             if entity_rect.colliderect(rect):
                 if frame_movement[1] > 0:  # Bottom
                     entity_rect.bottom = rect.top
+                    self.collisions["down"] = True
                 if frame_movement[1] < 0:  # Top
                     entity_rect.top = rect.bottom
+                    self.collisions["up"] = True
 
                 self.pos[1] = entity_rect.y
 
         # Terminal velocity for y-axis
         self.velocity[1] = min(5, self.velocity[1] + 0.1)
+
+        # Reset for falling or wall-jumping
+        if self.collisions["down"] or self.collisions["up"]:
+            self.velocity[1] = 0
 
     def render(self, surf):
         surf.blit(self.game.assets['player'], self.pos)
