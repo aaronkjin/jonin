9d5161d4e8fbdcf483a33c3bc8059c835c5fb569
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-05-17 15:58:52 -0700

    bug

--- game.py
+++ game.py
@@ -2,7 +2,7 @@
 import pygame
 import sys
 
-from scripts.entities import PhysicsEntity
+from scripts.entities import PhysicsEntity, Player
 from scripts.utils import load_image, load_images, Animation
 from scripts.tilemap import Tilemap
 from scripts.clouds import Clouds
@@ -39,7 +39,7 @@ class Game:
 
         self.clouds = Clouds(self.assets["clouds"], count=16)
 
-        self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
+        self.player = Player(self, (50, 50), (8, 15))
 
         self.tilemap = Tilemap(self, tile_size=16)
 
