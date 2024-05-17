30d41b436eb9ec9cd772863e63d7ad0ccb9d633c
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-05-17 15:57:29 -0700

    jump, run, idle animations

--- scripts/entities.py
+++ scripts/entities.py
@@ -25,8 +25,8 @@ class PhysicsEntity:
         # Action has changed, so change animation
         if action != self.action:
             self.action = action
-            self.animation = self.assets[self.e_type +
-                                         "/" + self.action].copy()
+            self.animation = self.game.assets[self.type +
+                                              "/" + self.action].copy()
 
     def update(self, tilemap, movement=(0, 0)):
         self.collisions = {"up": False, "down": False,
@@ -68,6 +68,11 @@ class PhysicsEntity:
 
                 self.pos[1] = entity_rect.y
 
+        if movement[0] > 0:  # Player moving right
+            self.flip = False
+        if movement[0] < 0:  # Player moving left
+            self.flip = True
+
         # Terminal velocity for y-axis
         self.velocity[1] = min(5, self.velocity[1] + 0.1)
 
@@ -75,10 +80,30 @@ class PhysicsEntity:
         if self.collisions["down"] or self.collisions["up"]:
             self.velocity[1] = 0
 
-    def render(self, surf, offset=(0, 0)):
-        # surf.blit(self.game.assets['player'],
-        #           (self.pos[0] - offset[0], self.pos[1] - offset[1]))
+        self.animation.update()
 
+    def render(self, surf, offset=(0, 0)):
         # If player faces left, animate towards left, and vice versa (only for x-axis)
         surf.blit(pygame.transform.flip(
-            self.animation.img(), self.flip, False))
+            self.animation.img(), self.flip, False), (self.pos[0] - offset[0] + self.anim_offset[0], self.pos[1] - offset[1] + self.anim_offset[1]))
+
+
+class Player(PhysicsEntity):
+    # Player class inherits all of physics entity's class plus more
+    def __init__(self, game, pos, size):
+        super().__init(game, "player", pos, size)
+        self.air_time = 0
+
+    def update(self, tilemap, movement=(0, 0)):
+        super().update(tilemap, movement=movement)
+
+        self.air_time += 1
+        if self.collisions["down"]:
+            self.air_time = 0
+
+        if self.air_time > 4:
+            self.set_action("jump")
+        elif movement[0] != 0:
+            self.set_action("run")
+        else:
+            self.set_action("idle")
