c4dd089e447226b2f8b2b5413b2196ab29689342
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-05-20 18:05:53 -0700

    remove pre-placed tiles

--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -14,13 +14,6 @@ class Tilemap:
         self.tilemap = {}
         self.offgrid_tiles = []
 
-        for i in range(10):
-            # Tile as a dictionary of metadata
-            self.tilemap[str(
-                3 + i) + ";10"] = {"type": "grass", "variant": 1, "pos": (3 + i, 10)}
-            self.tilemap["10;" + str(
-                5 + i)] = {"type": "stone", "variant": 1, "pos": (10, i + 5)}
-
     def tiles_around(self, pos):
         tiles = []
         # Change pixel position to grid position
