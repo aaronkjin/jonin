4c4de9ba1cda292609639e46e80b9b011b7960ba
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-05-21 21:16:02 -0700

    particle constructor

---
+++ scripts/particle.py
@@ -0,0 +1,8 @@
+class Particle:
+    def __init__(self, game, p_type, pos, velocity=[0, 0], frame=0):
+        self.game = game
+        self.type = p_type
+        self.pos = list(pos)
+        self.velocity = list(velocity)
+        self.animation = self.game.assets["particle/" + p_type].copy()
+        self.animation.frame = frame
