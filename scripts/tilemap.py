65c93f6bb2d722b954322a6691fb14f4d0069569
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-05-21 20:49:02 -0700

    autotile mapping

--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -5,8 +5,24 @@ import os
 # All permutations of -1 to 1 for player's position
 NEIGHBOR_OFFSETS = [(-1, 0), (-1, -1), (0, -1), (1, -1),
                     (1, 0), (0, 0), (-1, 1), (0, 1), (1, 1)]
+
 # A set of tiles that interact with physics
 PHYSICS_TILES = {"grass", "stone"}
+
+# Constants for autotiling
+AUTOTILE_TYPES = {"grass", "stone"}
+AUTOTILE_MAP = {
+    # To ensure same order every time
+    tuple(sorted([(1, 0), (0, 1)])): 0,
+    tuple(sorted([(1, 0), (0, 1), (-1, 0)])): 1,
+    tuple(sorted([(-1, 0), (0, 1)])): 2,
+    tuple(sorted([(-1, 0), (0, -1), (0, 1)])): 3,
+    tuple(sorted([(-1, 0), (0, -1)])): 4,
+    tuple(sorted([(-1, 0), (0, -1), (1, 0)])): 5,
+    tuple(sorted([(1, 0), (0, -1)])): 6,
+    tuple(sorted([(1, 0), (0, -1), (0, 1)])): 7,
+    tuple(sorted([(1, 0), (-1, 0), (0, -1)])): 8,
+}
 
 
 class Tilemap:
@@ -61,6 +77,16 @@ class Tilemap:
                     tile["pos"][0] * self.tile_size, tile["pos"][1] * self.tile_size, self.tile_size, self.tile_size))
 
         return rects
+
+    def autotile(self):
+        for loc in self.tilemap:
+            tile = self.tilemap[loc]
+            neighbors = set()
+
+            for shift in [(1, 0), (-1, 0), (0, -1), (0, 1)]:
+                check_loc = str(tile["pos"][0] + shift[0]) + \
+                    ";" + str(tile["pos"][1] + shift[1])
+                if check_loc in self.tilemap:
 
     def render(self, surf, offset=(0, 0)):
         # Render logic for off-grid lists
