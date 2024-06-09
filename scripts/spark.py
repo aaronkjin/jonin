bd33b70aa02af86e6028ed50a1fa824faf17a409
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-06-09 00:55:52 -0700

    init spark render

--- scripts/spark.py
+++ scripts/spark.py
@@ -13,3 +13,14 @@ class Spark:
         # Polar to Cartesian
         self.pos[0] += math.cos(self.angle) * self.speed
         self.pos[1] += math.sin(self.angle) * self.speed
+
+        # Use speed itself as a timer
+        self.speed = max(0, self.speed - 0.1)
+        return not self.speed
+
+    def render(self, surf, offset=(0, 0)):
+        render_points = [
+            (self.pos[0] + math.cos(self.angle) * self.speed)
+        ]
+
+        pygame.draw.polygon(surf, (255, 255, 255), render_points)
