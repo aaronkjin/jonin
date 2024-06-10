0b8a35ded7b491e9853ff312ed2ff272a7e1105a
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-06-09 19:10:59 -0700

    fixed game attribute for game

--- game.py
+++ game.py
@@ -146,7 +146,7 @@ class Game:
                             speed = random.random() * 5
                             self.sparks.append(
                                 Spark(self.player.rect().center, angle, 2 + random.random()))
-                            self.particles.append(Particle(self.game, "particle", self.player.rect().center, velocity=[
+                            self.particles.append(Particle(self, "particle", self.player.rect().center, velocity=[
                                                   math.cos(angle + math.pi) * speed * 0.5, math.sin(angle + math.pi) * speed * 0.5], frame=random.randint(0, 7)))
 
             # Self-managing for updating and removing sparks
