7aba624ddff2614f44b3894a1014a75ed2d0048e
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-06-09 18:48:46 -0700

    more trigs for more sides

--- scripts/spark.py
+++ scripts/spark.py
@@ -21,8 +21,18 @@ class Spark:
     def render(self, surf, offset=(0, 0)):
         # Diamond-shaped sparks via trigonometry
         render_points = [
+            # Forward
             (self.pos[0] + math.cos(self.angle) * self.speed * 3 - offset[0],
-             self.pos[1] + math.sin(self.angle) * self.speed * 3 - offset[1])
+             self.pos[1] + math.sin(self.angle) * self.speed * 3 - offset[1]),
+            # Side
+            (self.pos[0] + math.cos(self.angle + math.pi * 0.5) * self.speed * 0.5 - offset[0],
+             self.pos[1] + math.sin(self.angle + math.pi * 0.5) * self.speed * 0.5 - offset[1]),
+            # Backward
+            (self.pos[0] + math.cos(self.angle + math.pi) * self.speed * 3 - offset[0],
+             self.pos[1] + math.sin(self.angle + math.pi) * self.speed * 3 - offset[1]),
+            # Side
+            (self.pos[0] + math.cos(self.angle - math.pi * 0.5) * self.speed * 0.5 - offset[0],
+             self.pos[1] + math.sin(self.angle - math.pi * 0.5) * self.speed * 0.5 - offset[1]),
         ]
 
         pygame.draw.polygon(surf, (255, 255, 255), render_points)
