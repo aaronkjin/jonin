28bd75f101149145a9e95f17ba61607e3301c4c5
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-06-10 22:42:25 -0700

    temp same effects as player

--- scripts/entities.py
+++ scripts/entities.py
@@ -161,6 +161,14 @@ class Enemy(PhysicsEntity):
         # Player's hitbox collides into enemy's hitbox while they are dashing
         if abs(self.game.player.dashing) >= 50:
             if self.rect().colliderect(self.game.player.rect()):
+                for _ in range(30):
+                    # Random angle in circle
+                    angle = random.random() * math.pi * 2
+                    speed = random.random() * 5
+                    self.sparks.append(
+                        Spark(self.player.rect().center, angle, 2 + random.random()))
+                    self.particles.append(Particle(self, "particle", self.player.rect().center, velocity=[
+                                          math.cos(angle + math.pi) * speed * 0.5, math.sin(angle + math.pi) * speed * 0.5], frame=random.randint(0, 7)))
                 return True
 
     def render(self, surf, offset=(0, 0)):
